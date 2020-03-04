a15dabc93a3330224918e24e5c1029b560c77928
Your Name <you@example.com>
2020-03-04 23:45:17 +0530

    added the state

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
+    state = {
+        count:1
+    };
     render() {
         return (
             <React.Fragment>
-                <h1>Hello World</h1>
+                <span>{this.formatCount()}</span>
                 <button>Increment</button>
             </React.Fragment>
-        )
+        );
     }
+
+formatCount(){
+    const {count} = this.state;
+    return this.state.count === 0 ? 'zero' : this.state.count;
+}
 }
 export default Counter

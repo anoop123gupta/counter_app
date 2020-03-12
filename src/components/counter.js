1c785ab7b7f573cb1c45e610f36f761f4cd684bf
Your Name <you@example.com>
2020-03-12 23:56:48 +0530

    rendering classes dynamically

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count:1
+        count:0,
+
     };
     render() {
         return (
-            <React.Fragment>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
-            </React.Fragment>
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+            </div >
         );
     }
 
+getBadgeClasses(){
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary"
+    return classes;
+
+}
 formatCount(){
     const {count} = this.state;
     return this.state.count === 0 ? 'zero' : this.state.count;
 }
 }
-export default Counter
+export default Counter;
+
+
+// Rendering classes dynamically
